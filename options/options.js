e20b14ec4652c0846056ab533cf13afcd0ead120
Edward Grigorovich <edward.grigorovich@crowdbotics.com>
2023-05-14 21:35:57 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "edwards-splash-scre-83237",
-  url: "https://edwards-splash-scre-83237.botics.co",
-  api: "https://edwards-splash-scre-83237.botics.co/api/v1"
-}
+  "name": "edwards-splash-scre-83237",
+  "url": "https://edwards-splash-scre-83237.botics.co",
+  "api": "https://edwards-splash-scre-83237.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
