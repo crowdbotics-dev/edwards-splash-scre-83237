058586b2643ba5dfbafe113af7aa7fadb7c63a91
Crowdbotics <engineering@crowdbotics.com>
2023-05-15 01:06:51 +0000

    Initial commit.

---
+++ backend/edwards_splash_scre_83237/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for edwards_splash_scre_83237 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edwards_splash_scre_83237.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "edwards-splash-scre-83237",
+  url: "https://edwards-splash-scre-83237.botics.co",
+  api: "https://edwards-splash-scre-83237.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
